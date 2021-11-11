68b8fb08c366508351ccb4b653eca1a6af721b1d
matthew <matthewdgould1@gmail.com>
2021-11-10 23:49:49 -0500

    Added cache associativity to all algorithms and lease generation scripts

--- software/CLAM/run.sh
+++ software/CLAM/run.sh
@@ -1,13 +1,14 @@
 rm -f leases/*_*_lease*
-unset rate multi_level seed
+unset rate multi_level seed ways
 
 #get options
-while getopts "m:r:s:" option; do
+while getopts "m:r:s:w" option; do
     case "$option" in
         r ) rate="$OPTARG";;
         s ) seed="$OPTARG";;
         m ) multi_level="$OPTARG";;
-        * ) exit 1;;
+       	w ) ways="$OPTARG";;
+		* ) exit 1;;
     esac
 done
 #set defaults if not used
@@ -18,23 +19,23 @@ multi_level=${multi_level-no}
 
 
 if [[ "$multi_level" == "yes" ]]; then
-	 make run_all_small_multi_level CAPACITY=512 RATE=$rate SEED=$seed 
+	ways=${ways-512}
+	make run_all_small_multi_level CAPACITY=512 RATE=$rate SEED=$seed WAYS=$ways
 	 mv leases/*_prl*  "./leases/128_llt_entries/512blocks_512ways_PRL/"
 	 	mv leases/*_c-shel* "./leases/128_llt_entries/512blocks_512ways_C-SHEL/"
 	 	 mv leases/*_shel* "./leases/128_llt_entries/512blocks_512ways_SHEL/"
 	 	 mv leases/*_clam* "./leases/128_llt_entries/512blocks_512ways_CLAM/"
-	 make run_all_medium_multi_level CAPACITY=512 RATE=$rate SEED=$seed 
+	 make run_all_medium_multi_level CAPACITY=512 RATE=$rate SEED=$seed WAYS=$ways
 	  mv leases/*_prl*  "./leases/128_llt_entries/512blocks_512ways_PRL_medium/"
 	 	 mv leases/*_c-shel* "./leases/128_llt_entries/512blocks_512ways_C-SHEL_medium/"
 	 	 mv leases/*_shel_* "./leases/128_llt_entries/512blocks_512ways_SHEL_medium/"
 	 	 mv leases/*_clam_* "./leases/128_llt_entries/512blocks_512ways_CLAM_medium/"
-	 make run_all_large_multi_level CAPACITY=512 RATE=$rate SEED=$seed
+	 make run_all_large_multi_level CAPACITY=512 RATE=$rate SEED=$seed WAYS=$ways
 	 mv leases/*_prl*  "./leases/128_llt_entries/512blocks_512ways_PRL_large/"
 		mv leases/*_c-shel* "./leases/128_llt_entries/512blocks_512ways_C-SHEL_large/"
 	 	mv leases/*_shel* "./leases/128_llt_entries/512blocks_512ways_SHEL_large/"
-	mv leases/*_clam* "./leases/128_llt_entries/512blocks_512ways_CLAM_large/"
-#		
-#make run_all_extra_large_multi_level CAPACITY=512 RATE=$rate SEED=$seed WAYS=512
+	mv leases/*_clam* "./leases/128_llt_entries/512blocks_512ways_CLAM_large/"	
+#make run_all_extra_large_multi_level CAPACITY=512 RATE=$rate SEED=$seed WAYS=$ways
 #mv leases/*_c_shel* "./leases/128_llt_entries/512blocks_512ways_C-SHEL_extra_large/"
 #mv leases/*_shel* "./leases/128_llt_entries/512blocks_512ways_SHEL_extra_large/"
 #mv leases/*_clam* "./leases/128_llt_entries/512blocks_512ways_CLAM_extra_large/"
@@ -43,22 +44,23 @@ if [[ "$multi_level" == "yes" ]]; then
 
 
  else 
-make run_all_small CAPACITY=128 RATE=$rate SEED=$seed
+	ways=${ways-128}
+make run_all_small CAPACITY=128 RATE=$rate SEED=$seed WAYS=$ways
 	mv leases/*_prl*  "./leases/128_llt_entries/128blocks_128ways_PRL/"
-		#mv leases/*_c-shel* "./leases/128_llt_entries/128blocks_128ways_C-SHEL/"
+		mv leases/*_c-shel* "./leases/128_llt_entries/128blocks_128ways_C-SHEL/"
 		mv leases/*_shel* "./leases/128_llt_entries/128blocks_128ways_SHEL/"
 		mv leases/*_clam* "./leases/128_llt_entries/128blocks_128ways_CLAM/"
-	 make run_all_medium CAPACITY=128 RATE=$rate SEED=$seed 
+	 make run_all_medium CAPACITY=128 RATE=$rate SEED=$seed WAYS=$ways
 	 mv leases/*_prl*  "./leases/128_llt_entries/128blocks_128ways_PRL_medium/"
 	 	mv leases/*_c-shel* "./leases/128_llt_entries/128blocks_128ways_C-SHEL_medium/"
 	 	mv leases/*_shel* "./leases/128_llt_entries/128blocks_128ways_SHEL_medium/"
 	 	mv leases/*_clam* "./leases/128_llt_entries/128blocks_128ways_CLAM_medium/"
-	make run_all_large CAPACITY=128 RATE=$rate SEED=$seed 
+	make run_all_large CAPACITY=128 RATE=$rate SEED=$seed WAYS=$ways
 	mv leases/*_prl*  "./leases/128_llt_entries/128blocks_128ways_PRL_large/"
 	 	mv leases/*_c-shel* "./leases/128_llt_entries/128blocks_128ways_C-SHEL_large/"
 	 	mv leases/*_shel* "./leases/128_llt_entries/128blocks_128ways_SHEL_large/"
 	 	mv leases/*_clam* "./leases/128_llt_entries/128blocks_128ways_CLAM_large/"
-#make run_all_extra_large CAPACITY=128 RATE=$rate SEED=$seed 
+#make run_all_extra_large CAPACITY=128 RATE=$rate SEED=$seed WAYS=$ways
 #mv leases/*_c_shel* "./leases/128_llt_entries/128blocks_128ways_C-SHEL_extra_large/"
 #mv leases/*_shel* "./leases/128_llt_entries/128blocks_128ways_SHEL_extra_large/"
 #mv leases/*_clam* "./leases/128_llt_entries/128blocks_128ways_CLAM_extra_large/"
--- software/CLAM/src/main.rs
+++ software/CLAM/src/main.rs
@@ -81,7 +81,7 @@ fn main(){
     let search_string=matches.value_of("INPUT").unwrap().to_lowercase();
     let cap= re.captures(&*search_string).unwrap();
 
-/*
+
 //if associativity not specified, set as fully associative
     let num_ways:u64;
     if matches.value_of("Set Associativity").unwrap().parse::<i64>().unwrap()==-1{
@@ -89,14 +89,16 @@ fn main(){
     }
     else {
         num_ways=matches.value_of("Set Associativity").unwrap().parse::<u64>().unwrap();
-    }*/
+    }
+    //get mask for set bits
+    let set_mask=(cache_size as f64 /num_ways as f64) as u32-1;
 
 //generate distributions
-       let (ri_hists,samples_per_phase,misses_from_first_access,sample_rate) = cshel::io::build_ri_hists(matches.value_of("INPUT").unwrap(),cshel);
+       let (ri_hists,samples_per_phase,misses_from_first_access,sample_rate) = cshel::io::build_ri_hists(matches.value_of("INPUT").unwrap(),cshel,set_mask);
    //generates PRL
    if prl {
     //generate bins
-    let (binned_ri_distributions,binned_freqs,bin_width) = cshel::io::get_binned_hists(matches.value_of("INPUT").unwrap(),perl_bin_num);
+    let (binned_ri_distributions,binned_freqs,bin_width) = cshel::io::get_binned_hists(matches.value_of("INPUT").unwrap(),perl_bin_num,set_mask);
   println!("{:?}",binned_ri_distributions);
     //compose output file name
     //this panic here avoids the almost certain panic that will result from running PRL on multi phase sampling files
@@ -107,7 +109,7 @@ fn main(){
     output_file_name=format!("{}/{}_{}_{}",matches.value_of("OUTPUT").unwrap(),&cap[2],"prl","leases");
     //generate prl leases
     let (leases, dual_leases, lease_hits,trace_length) = cshel::lease_gen::prl(bin_width,
-        &ri_hists,&binned_ri_distributions,&binned_freqs,sample_rate,cache_size,discretize_width,&samples_per_phase,verbose,debug).unwrap();
+        &ri_hists,&binned_ri_distributions,&binned_freqs,sample_rate,cache_size,discretize_width,&samples_per_phase,verbose,debug,set_mask).unwrap();
     println!("running PRL");
     let lease_vectors=cshel::io::dump_leases(leases,dual_leases,lease_hits,trace_length,&output_file_name[..],sample_rate,misses_from_first_access);
     let output_lease_file_name=format!("{}/{}_{}_{}",matches.value_of("OUTPUT").unwrap(),&cap[2],"prl","lease.c");
@@ -118,7 +120,7 @@ fn main(){
     output_file_name=format!("{}/{}_{}_{}",matches.value_of("OUTPUT").unwrap(),&cap[2],&cap[1],"leases");
        //generates based on input file phases, CLAM or SHEL 
    let (leases,dual_leases, lease_hits,trace_length) = cshel::lease_gen::shel_cshel(false,&ri_hists,cache_size,sample_rate,&samples_per_phase,discretize_width, 
-    verbose,debug).unwrap();
+    verbose,debug,set_mask).unwrap();
   let lease_vectors=cshel::io::dump_leases(leases,dual_leases,lease_hits,trace_length,&output_file_name[..],sample_rate,misses_from_first_access);
    let output_lease_file_name=format!("{}/{}_{}_{}",matches.value_of("OUTPUT").unwrap(),&cap[2],&cap[1],"lease.c");
    //generate lease file
@@ -128,7 +130,7 @@ fn main(){
     //generate leases
     println!("Running C-SHEL.");
      let (leases, dual_leases, lease_hits,trace_length) = cshel::lease_gen::shel_cshel(true,&ri_hists,cache_size,sample_rate,&samples_per_phase,discretize_width,
-        verbose,debug).unwrap();
+        verbose,debug,set_mask).unwrap();
        //compose output file name
     output_file_name=format!("{}/{}_{}_{}",matches.value_of("OUTPUT").unwrap(),&cap[2],"c-shel","leases");
       //output to file
