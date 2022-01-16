92d6026260aaf179ecf37d4f4489dd06f48c9aab
matthew <matthewdgould1@gmail.com>
2022-01-16 17:01:58 -0500

    modifiy path for gcc risc embedded to be non-user specific, expanded setup environment script to install all necessasry software besides Quartus, added readme shell

---
+++ README.md
@@ -0,0 +1,42 @@
+#Setup
+
+
+#Hardware
+
+##top subdir
+	bash functions
+
+##internal subdir
+
+##peripheral subdir
+
+##external subdir
+
+##include subdir
+
+##utilities subdir
+
+
+#Software
+
+##benchmark subdir
+	##bash functions 
+##fpga_proxy subdir
+	Proxy Commands
+	Makefile
+	Bash Scripts
+	Bash functions
+##CLAM subdir
+	lease policy software
+	Bash Scripts 
+	Bash functions
+
+#MATLAB_data_visualizations
+##lease_cache_tracking subdir
+	matlab scripts
+##cache_statistics subdir
+	matlab scripts
+## Bash functions and Scripts
+
+
+#Data Generation walkthrough
--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,35 @@
+echo "setting up research path variable"
 CLAM_path=$(pwd);
-echo "export CLAM_path=$CLAM_path" >> ~/.bashrc
+echo "export CLAM_path=$CLAM_path">>~/.bashrc
 
-echo "if [ -f $CLAM_path/CLAM_bash_functions.sh ]; then" >> ~/.bashrc
-echo "	. $CLAM_path/CLAM_bash_functions.sh" >> ~/.bashrc
+echo "adding bash functions to bashrc"
+echo "if [ -f $CLAM_path/CLAM_bash_functions.sh ]; then">> ~/.bashrc
+echo "	. $CLAM_path/CLAM_bash_functions.sh">> ~/.bashrc
 echo "fi" >> ~/.bashrc 
 
+echo "installing libpng12 which is necessasry for quartus to work"
+sudo add-apt-repository ppa:linuxuprising/libpng12
+sudo apt update
+sudo apt install libpng12-0
+
+echo "installing GNU RISCV-Embedded GCC"
+sudo apt install npm
+sudo npm install --global xpm@latest
+xpm install --global @xpack-dev-tools/riscv-none-embed-gcc@latest --verbose
+
+echo "setting USB blaster device files"
+cat > 51−usbblaster.rules<< EOF
+"# USB-Blaster"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6001", MODE="0666"
+"SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6002", MODE="0666"
+
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6003", MODE="0666"
+
+# USB-Blaster II
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6010", MODE="0666"
+SUBSYSTEM=="usb", ATTRS{idVendor}=="09fb", ATTRS{idProduct}=="6810", MODE="0666"
+EOF
+sudo mv ./51−usbblaster.rules /etc/udev/rules.d/51−usbblaster.rules
+
+echo "install rust and rust cargo package manager"
+curl https://sh.rustup.rs -sSf | sh
